26cfaef3c96c7cc61bb96287feddfe72e1283167
Antony Sastre <antony@thenewconcept.com>
2018-02-03 16:13:01 +0100

    Path fixes.

--- wp-content/themes/lapoint-living/footer.php
+++ wp-content/themes/lapoint-living/footer.php
@@ -2,13 +2,13 @@
       <div class="o-container o-container--large">
         <div class="c-flex-block">
           <div class="c-flex-block__cell">
-            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/images/lapoint-living-logo--white@2x.png" width="105" alt="Lapoint living logo  white@2x" />
+            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/lapoint-living-logo--white@2x.png" width="105" alt="Lapoint living logo  white@2x" />
           </div>
           <div class="c-flex-block__cell u-text-sans-serif">
             <p>Stay in touch:</p>
-            <p><img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/images/icon-phone.svg" alt="Icon phone" /> +47 46 63 97 42</p>
-            <p><img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/images/icon-email.svg" alt="Icon email" /> <a href="mailto:info@lapointcamps.com">info@lapointcamps.com</a></p>
-            <p><img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/images/social-media.png" alt="Social media" /></p>
+            <p><img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/icon-phone.svg" alt="Icon phone" /> +47 46 63 97 42</p>
+            <p><img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/icon-email.svg" alt="Icon email" /> <a href="mailto:info@lapointcamps.com">info@lapointcamps.com</a></p>
+            <p><img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/social-media.png" alt="Social media" /></p>
           </div>
         </div>
       </div>
